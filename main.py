4c9faff1869e366e441960f4d005d1190f0d8654
ksg-dev <ksg.dev.data@gmail.com>
2024-06-17 15:41:12 -0500

    check for rain weather codes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 
+OWM_EndPoint = "https://api.openweathermap.org/data/2.5/forecast"
 api_key = "856b43ff1555aead490c92cbf0d86355"
 MY_LAT = 30.673290
 MY_LONG = -88.111153
@@ -8,18 +9,30 @@ parameters = {
     "lat": MY_LAT,
     "lon": MY_LONG,
     "appid": api_key,
+    "cnt": 4,
 }
 
-# url: https://api.openweathermap.org/data/2.5/forecast?lat=30.673290&lon=-88.111153&appid=856b43ff1555aead490c92cbf0d86355
+# url: https://api.openweathermap.org/data/2.5/forecast?lat=30.673290&lon=-88.111153&appid=856b43ff1555aead490c92cbf0d86355&cnt=4
 
-response = requests.get(url="https://api.openweathermap.org/data/2.5/forecast", params=parameters)
+response = requests.get(url=OWM_EndPoint, params=parameters)
 response.raise_for_status()
 data = response.json()
 
+weather_codes = [data["list"][i]["weather"][0]["id"] for i in range(0,4)]
 weather_id = data["list"][0]["weather"][0]["id"]
 weather_desc = data["list"][0]["weather"][0]["description"]
 
-print(weather_id)
-print(weather_desc)
+# print(weather_id)
+# print(weather_desc)
+
+print(f"weather codes: {weather_codes}")
+
+will_rain = False
+for code in weather_codes:
+    if code < 700:
+        will_rain = True
+
+if will_rain:
+    print("Bring an umbrella")
 
 # print(data)
